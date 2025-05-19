71d5c8ad536c2ca3c01b9d6f230920956c1d8c71
amedeo <amedeo.alfonsi@yahoo.com>
2025-05-19 22:45:51 +0200

    Risolta regressione nello sfondo della pagina di assegnazione permessi

--- app/src/main/java/it/edu/iisfermisacconiceciap/safetyapp/ErrorScreen.kt
+++ app/src/main/java/it/edu/iisfermisacconiceciap/safetyapp/ErrorScreen.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -44,7 +45,7 @@ fun MissingPermScreen(
     )
 ) {
     SafetyAppTheme {
-        Surface {
+        Surface(Modifier.fillMaxSize()) {
             Column {
                 Text(
                     "Permessi mancanti",
