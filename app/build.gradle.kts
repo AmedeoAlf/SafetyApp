3868a409c1a126976aa5ba9632e7d4cebe9bfd06
amedeo <amedeo.alfonsi@yahoo.com>
2025-05-28 17:45:27 +0200

    Pulizia dei polling per il controllo di molte proprietà

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "it.edu.iisfermisacconiceciap.safetyapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
