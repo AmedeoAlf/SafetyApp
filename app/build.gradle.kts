923ded7613a8a02ba494a72ceef0633fa8784748
amedeo <amedeo.alfonsi@yahoo.com>
2025-09-14 22:38:16 +0200

    Bump a 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "it.edu.iisfermisacconiceciap.safetyapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.3"
+        versionCode = 5
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
